ba920b06cf64f3a15f64f07be1e713ddca911472
Ubuntu <ubuntu@ip-172-31-29-183.us-west-1.compute.internal>
2022-07-10 20:02:56 -0700

    Database Update

--- db/donttrip.sql
+++ db/donttrip.sql
@@ -1,14 +1,14 @@
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
   username varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL, 
+  id INT NOT NULL AUTO_INCREMENT, 
   password varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL, 
   email VARCHAR(100) NOT NULL UNIQUE KEY, 
-  id INT NOT NULL AUTO_INCREMENT, 
-  Unique KEY (id), 
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  last_login DATETIME DEFAULT CURRENT_TIMESTAMP, 
   email_verification_link VARCHAR(255) NOT NULL, 
   email_verified_at TIMESTAMP NULL, 
   tfaen INT, 
   tfa VARCHAR(255), 
+  UNIQUE KEY (id), 
   PRIMARY KEY (username)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
 CREATE TABLE IF NOT EXISTS all_login_attempts (
@@ -17,7 +17,11 @@ CREATE TABLE IF NOT EXISTS all_login_attempts (
   attempt_date DATETIME, 
   ip VARCHAR(255)
 );
-CREATE TABLE IF NOT EXISTS login_attempts LIKE all_login_attempts;
+CREATE TABLE IF NOT EXISTS page_visits (
+  browser VARCHAR(255) NOT NULL, 
+  visit_date DATETIME, 
+  ip VARCHAR(255)
+);
 CREATE TABLE IF NOT EXISTS password_reset_temp (
   email VARCHAR(250), 
   keyTO VARCHAR(255), 
